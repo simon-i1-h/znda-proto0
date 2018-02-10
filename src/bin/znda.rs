fe671939b6a52bfdb5b79153549054d7cb743387
Itoh Shimon <newbee@arrow.ocn.ne.jp>
2018-02-10 21:27:40 +0900

    コマンドラインパーサーを更新

--- src/bin/znda.rs
+++ src/bin/znda.rs
@@ -5,17 +5,16 @@
 // znda (--version|version)
 
 use std::env;
-use std::ffi::OsStr;
 
 enum OptParseErrorKind {
     Error
 }
 
 enum ProgMode {
-    RunCodePieces{ code_pieces: Vec<String> },
-    RunScript{ filenames: Vec<String> },
+    RunSnippet{ snippet: String },
+    RunScript{ path: String },
     PrintHelp,
-    PrintVersion
+    PrintVersion,
 }
 
 // TODO 悪い意味で富豪的なので直す
@@ -23,40 +22,29 @@ fn parse_opts<T>(opts: T) -> Result<ProgMode, OptParseErrorKind>
     where T: IntoIterator,
           T::Item: AsRef<str>
 {
-//    let mode = Node; TODO help_flagなどを使わずにProgModeを使う?
     let mut into_opt_e = false;
-    let mut pieces = Vec::new();
-    let mut filenames = Vec::new();
 
     for s in opts.into_iter() {
         let s = s.as_ref();
+
+        if into_opt_e {
+            return Ok(ProgMode::RunSnippet{ snippet: s.to_string() });
+        }
 
         match s {
             "-e" => into_opt_e = true,
             "--help" | "help" => return Ok(ProgMode::PrintHelp),
             "--version" | "version" => return Ok(ProgMode::PrintVersion),
-            o => {
-                if into_opt_e {
-                    pieces.push(s.to_string());
-                    into_opt_e = false;
-                } else if !pieces.is_empty() {
-                    filenames.push(s.to_string());
-                }
-            }
+            o => return Ok(ProgMode::RunScript{ path: o.to_string() }),
         }
     }
 
-    if into_opt_e {
-        // TODO return error
-        unimplemented!()
-    }
-
-    unimplemented!()
+    Err(OptParseErrorKind::Error)
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let progname = &args[0];
 
-    let res = parse_opts(&args[1..]).unwrap_or_else(|_| panic!());
+    let mode = parse_opts(&args[1..]).unwrap_or_else(|_| panic!());
 }
